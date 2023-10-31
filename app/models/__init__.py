5f1f40d822f9b261873c0276272a2009201298d2
Josh <jzumbrunn@yahoo.com>
2023-10-31 18:03:19 -0400

    models and relationships

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,3 +3,5 @@ from .user import User
 from .db import environment, SCHEMA
 from .guitar import Guitar
 from .guitar_image import GuitarImage
+from .cart_item import CartItem
+from .shopping_cart import ShoppingCart
---
+++ app/models/cart_item.py
@@ -0,0 +1,33 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+
+class CartItem(db.Model):
+    __tablename__ = 'cart_items'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+
+    id = db.Column(db.Integer, primary_key=True)
+    cart_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('shopping_carts.id')))
+    guitarId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('guitars.id')))
+    quantity = db.Column(db.Integer, default=1)
+
+    shopping_cart = db.relationship(
+        'ShoppingCart',
+        back_populates='cart_items'
+    )
+
+    guitar = db.relationship(
+        'Guitar',
+        back_populates='cart_items'
+    )
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'cart_id': self.cart_id,
+            'guitar_id': self.guitar_id,
+            'quantity': self.quantity,
+            'guitar': self.guitar.to_dict()
+        }
