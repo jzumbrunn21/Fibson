790cb669b44b91bde16aa6c07a6dd4ec0ba91996
Josh <jzumbrunn@yahoo.com>
2023-11-08 14:20:02 -0500

    splash css

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -24,6 +24,47 @@ const SplashPage = () => {
           </NavLink>
         </div>
       </div>
+      <div className="info-boxes">
+        <div className="black-box">
+          <img
+            src={
+              "https://images.ctfassets.net/m8onsx4mm13s/4qm6G6cNU4mSAb566UuIJ1/5dfd9c110728306b565b59671a95cc83/In-Time-For-Holidays__2_.png"
+            }
+            alt="Free Shipping"
+            className="black-box-image"
+          />
+          <div className="black-box-deets1">
+            <h3>Free Shipping</h3>
+            <h5>On online orders over $50.</h5>
+          </div>
+        </div>
+        <div className="black-box">
+          <img
+            src={
+              "https://images.ctfassets.net/m8onsx4mm13s/6IvzKzqfqdVRKgZHjBqFIk/de259c13ae15133900b72c494b76c694/Lifetime-Warranty__2_.png"
+            }
+            alt="Lifetime Warranty"
+            className="black-box-image"
+          />
+          <div className="black-box-deets2">
+            <h3>Lifetime Warranty</h3>
+            <h5>Free, limited warranty on material and workmanship</h5>
+          </div>
+        </div>
+        <div className="black-box">
+          <img
+            src={
+              "https://images.ctfassets.net/m8onsx4mm13s/24dB0X9zp0nHGgGooFJUGn/2d7bed365abe0a24ffa79eb21b4e1f3a/Financing-Available__2_.png"
+            }
+            alt="Financing Available"
+            className="black-box-image"
+          />
+          <div className="black-box-deets3">
+            <h3>Financing Available</h3>
+            <h5>Options available through your wallet at checkout.</h5>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
