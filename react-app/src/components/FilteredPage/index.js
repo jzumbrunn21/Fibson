ec1e2fbb4fd6bb1a88af6056ad6985c9bdfd2e70
Josh <jzumbrunn@yahoo.com>
2023-11-09 05:02:08 -0500

    update/create id fixes for rendering filtered page

--- react-app/src/components/FilteredPage/index.js
+++ react-app/src/components/FilteredPage/index.js
@@ -21,43 +21,50 @@ const FilteredPage = () => {
 
   return (
     <>
-      <h2>Howdy</h2>
+      <h2>Check out our {category} collection:</h2>
       <div className="all-listings-container">
-        {listings.map((listing) => (
-          <Link
-            to={`/listings/${listing.guitar.id}`}
-            key={listing.guitar.id}
-            className="single-listing-container"
-          >
-            <div className="all-listing-image">
-              {/* <img
+        {listings.map(
+          (listing) =>
+            listing.guitar && (
+              <Link
+                to={`/listings/${listing.guitar.id}`}
+                key={listing.guitar.id}
+                className="single-listing-container"
+              >
+                <div className="all-listing-image">
+                  {/* <img
                 src={listing.images[0]}
                 alt={`${listing.guitar.make}, ${listing.guitar.model}`}
               /> */}
-              <Carousel
-                showThumbs={false}
-                showArrows={false}
-                autoPlay={true}
-                showIndicators={false}
-                interval={5000}
-                infiniteLoop={true}
-              >
-                {listing.images &&
-                  listing.images.map((image, index) => (
-                    <img key={index} src={image} alt={listing.guitar.model} />
-                  ))}
-              </Carousel>
-            </div>
-            <div className="all-listings-info-container">
-              <h3>
-                {listing.guitar.year} {listing.guitar.make}{" "}
-                {listing.guitar.model}
-              </h3>
-              <h5>{listing.guitar.color}</h5>
-              <h5>${listing.guitar.price}</h5>
-            </div>
-          </Link>
-        ))}
+                  <Carousel
+                    showThumbs={false}
+                    showArrows={false}
+                    autoPlay={true}
+                    showIndicators={false}
+                    interval={5000}
+                    infiniteLoop={true}
+                  >
+                    {listing.images &&
+                      listing.images.map((image, index) => (
+                        <img
+                          key={index}
+                          src={image}
+                          alt={listing.guitar.model}
+                        />
+                      ))}
+                  </Carousel>
+                </div>
+                <div className="all-listings-info-container">
+                  <h3>
+                    {listing.guitar.year} {listing.guitar.make}{" "}
+                    {listing.guitar.model}
+                  </h3>
+                  <h5>{listing.guitar.color}</h5>
+                  <h5>${listing.guitar.price}</h5>
+                </div>
+              </Link>
+            )
+        )}
       </div>
     </>
   );
