e0c96092b5cfb637f47def76cbbf19271c630733
Josh <jzumbrunn@yahoo.com>
2023-11-12 10:22:29 -0500

    small fixes

--- react-app/src/components/GuitarDetail/index.js
+++ react-app/src/components/GuitarDetail/index.js
@@ -92,25 +92,25 @@ const GuitarDetail = () => {
           <h2>Specs</h2>
           <div className="specs">
             <div>
-              <h3>Materials</h3>
-              <p>Type: {listing[0].guitar.guitar_type}</p>
-              <p>Body Type: {listing[0].guitar.body_type}</p>
-              <p>Wood: {listing[0].guitar.wood_type}</p>
+              <h2>Materials</h2>
+              <h4>Type: {listing[0].guitar.guitar_type}</h4>
+              <h4>Body Type: {listing[0].guitar.body_type}</h4>
+              <h4>Wood: {listing[0].guitar.wood_type}</h4>
             </div>
             <div className="vert-line"></div>
             <div>
-              <h3>Pickups</h3>
-              <p>Pickup Type: {listing[0].guitar.pickup_type}</p>
-              <p>Pickguard: {listing[0].guitar.pickguard ? "Yes" : "No"}</p>
-              <p>Pickup Selector: {listing[0].guitar.pickup_selector}</p>
+              <h2>Pickups</h2>
+              <h4>Pickup Type: {listing[0].guitar.pickup_type}</h4>
+              <h4>Pickguard: {listing[0].guitar.pickguard ? "Yes" : "No"}</h4>
+              <h4>Pickup Selector: {listing[0].guitar.pickup_selector}</h4>
             </div>
             <div className="vert-line"></div>
             <div>
-              <h3>Neck</h3>
-              <p>Joint: {listing[0].guitar.joint_type}</p>
-              <p>Fretboard Wood: {listing[0].guitar.fretboard_material}</p>
-              <p>Fret Count: {listing[0].guitar.frets}</p>
-              <p>Inlay Design: {listing[0].guitar.inlays}</p>
+              <h2>Neck</h2>
+              <h4>Joint: {listing[0].guitar.joint_type}</h4>
+              <h4>Fretboard Wood: {listing[0].guitar.fretboard_material}</h4>
+              <h4>Fret Count: {listing[0].guitar.frets}</h4>
+              <h4>Inlay Design: {listing[0].guitar.inlays}</h4>
             </div>
           </div>
         </div>
